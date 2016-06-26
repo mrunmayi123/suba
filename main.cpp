1e973f70df460e4c35ae2752a05b9ff1e14be4a5
mrunmayi123 <mrunmayi.parchure@gmail.com>
2016-06-26 11:52:01 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// GIT-Labor
+// main.h
+
+////////////////////////////////////////////////////////////////////////////////
+// Header-Dateien
+#include <iostream>		// Header für die Standard-IO-Objekte (z.B. cout, cin)
+#include <stdlib.h>
+// TODO: Fügen Sie hier weitere benötigte Header-Dateien der
+// Standard-Bibliothek ein z.B.
+// #include <string>
+
+using namespace std;	// Erspart den scope vor Objekte der
+						// C++-Standard-Bibliothek zu schreiben
+						// z.B. statt "std::cout" kann man "cout" schreiben
+
+// Inkludieren Sie hier die Header-Files Ihrer Klassen, z.B.
+// #include "CFraction.h"
+
+
+// Hauptprogramm
+// Dient als Testrahmen, von hier aus werden die Klassen aufgerufen
+int main (void)
+{
+    // TODO: Fügen Sie ab hier Ihren Programmcode ein
+	cout << "Mand2_MMP gestarted." << endl << endl;
+
+	return 0;
+}
